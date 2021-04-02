f7e8bcaf31861e1392efee216fac01a9c36a45fc
utpal223 <ukr4edu@gmail.com>
2021-04-02 16:07:34 +0530

    ui final & fn stg 1

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var lastInp = false
+    var dot=false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onDigit(view: View) {
+
+        txtv.append((view as Button).text)
+        lastInp=true
+
+    }
+
+
+
+    fun onOperator(view: View) {}
+
+
+
+    fun onDecimalPoint(view: View) {
+
+        if(lastInp && !dot){
+            txtv.append(".")
+            dot=true
+            lastInp=false
+        }
+    }
+
+
+
+    fun onClear(view: View) {
+        txtv.text = ""
+        dot=false
+        lastInp=false
+    }
+
+
+
+    fun onEqual(view: View) {}
+
+
+
+
 }
+
